2e23a563970df08d9850dc3f5a7de9a2f8862ab2
Adrian Milsap <adrian@hirobius.com>
2021-04-28 21:05:25 -0700

    submit push

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
 import { withAuth0 } from '@auth0/auth0-react';
-import MyFavoriteBooks from './MyFavoriteBooks'
-import Login from './Login'
-import Profile from './Profile'
+import MyFavoriteBooks from './MyFavoriteBooks';
+import Login from './Login';
+import Profile from './Profile';
+import axios from 'axios';
 
 import {
   BrowserRouter as Router,
@@ -14,6 +15,29 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books: [],
+      name: ''
+    }
+  }
+
+  getAllBooks = async (e) => {
+    e.preventDefault();
+    const SERVER = 'http://localhost:3002/';
+    try {
+      const books = await axios.get(`${SERVER}/books`, {params: { name: this.state.name }});
+      console.log(books.data)
+      this.setState({ books: books.data });
+
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  updateName = (name) => this.setState({ name });
 
   render() {
     const { isAuthenticated } = this.props.auth0;
@@ -27,7 +51,7 @@ class App extends React.Component {
               <Route exact path="/">
                 {isAuthenticated ? <MyFavoriteBooks /> : <Login />}
               </Route >
-              <Route exact path="/profile"><Profile /></Route>
+              <Route exact path="/profile"><Profile getAllBooks={this.getAllBooks} updateName={this.updateName}/></Route>
             </Switch>
             <Footer />
           </IsLoadingAndError>
